b43391e425c1238c040cfcbba3a1526635fd855c
kartik <kartik.kumar53@gmail.com>
2023-07-05 16:24:46 +0530

    integrate docker container

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -21,6 +21,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
